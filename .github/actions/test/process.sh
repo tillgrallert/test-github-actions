0cce6d370e5cc62427cbe809a96973feb3c12766
Till Grallert <till.grallert@fu-berlin.de>
2020-12-01 13:14:52 +0100

    added a basic test XSLT that should

--- .github/actions/test/process.sh
+++ .github/actions/test/process.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-REGEX="*TEIP5.xml"
 for f in `ls tei/*TEIP5.xml`
 do
-  echo "Checking $f"
-  if [[ $f =~ $REGEX ]]
-  then
-    echo "Converting $f"
-    saxon -s:"$f" -xsl:xslt/tei_generate-xml-id.xsl -o:"$f"
+  echo "Converting $f"
+  # saxon -s:"$f" -xsl:xslt/tei_generate-xml-id.xsl -o:"$f"
+  saxon -s:"$f" -xsl:xslt/test.xsl
   fi
 done
