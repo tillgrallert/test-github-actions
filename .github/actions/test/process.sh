4aec9401ac0b36f148c796054cc5e8a21d3bbf6f
Till Grallert <till.grallert@fu-berlin.de>
2020-12-03 17:55:22 +0100

    improved documentation

--- .github/actions/test/process.sh
+++ .github/actions/test/process.sh
@@ -2,9 +2,12 @@
 
 for f in `ls tei/*TEIP5.xml`
 do
-  echo "Converting $f"
+  echo "First test: generate a TXT file"
   # this very basic test runs as expected
   saxon -s:"$f" -xsl:xslt/test.xsl
   # try and run XSLT to generate @xml:id on all new nodes
-  saxon -s:"$f" -xsl:xslt/tei_generate-xml-id.xsl -o:test_output.xml p_id-editor=#github
+  # this works when the output is specified with a fixed file name
+  # it currently fails when I attempt to overwrite the input file with `-o:"$f"`
+  echo "Second test: transform the input XML"
+  saxon -s:"$f" -xsl:xslt/tei_generate-xml-id.xsl -o:test_output.xml p_id-editor=github
 done
