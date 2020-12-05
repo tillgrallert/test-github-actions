edc4a8f79a5376f60ad6c1ba8b4c61614c9532e6
Till Grallert <till.grallert@fu-berlin.de>
2020-12-05 21:37:38 +0100

    corrected path to authority file

--- .github/actions/update-arabic-columns/process.sh
+++ .github/actions/update-arabic-columns/process.sh
@@ -2,5 +2,5 @@
 for f in `ls tei/jaraid_master*.xml`
 do
   echo "Update the Arabic columns in the master with data from the authority file"
-  saxon -s:"$f" -xsl:xslt/tei_add-arabic-columns.xsl -o:"$f" p_url-authority='authority-files/jaraid_authority-file.TEIP5.xml'
+  saxon -s:"$f" -xsl:xslt/tei_add-arabic-columns.xsl -o:"$f" p_url-authority='../authority-files/jaraid_authority-file.TEIP5.xml'
 done
