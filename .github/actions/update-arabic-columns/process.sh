56992283773189e230f3e66d3d43dd4fd0e34c99
Till Grallert <till.grallert@fu-berlin.de>
2020-12-05 13:15:07 +0100

    moved to correct folder

--- .github/actions/update-arabic-columns/process.sh
+++ .github/actions/update-arabic-columns/process.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for f in `ls tei/jaraid_master.TEIP5.xml`
+for f in `ls tei/jaraid_master*.xml`
 do
   echo "Update the Arabic columns in the master with data from the authority file"
   saxon -s:"$f" -xsl:xslt/tei_add-arabic-columns.xsl -o:"$f"
