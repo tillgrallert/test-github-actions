a13cd358a28591f604bf674e6bd76aca73349cec
Till Grallert <till.grallert@fu-berlin.de>
2020-12-18 17:29:00 +0100

    added parameters to set the correct output directory

--- .github/actions/attributes-bibliographic-metadata/process.sh
+++ .github/actions/attributes-bibliographic-metadata/process.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# this selector is ignorant of the triggering file. The action will run on all files not just the ones recently changed
 for f in `ls tei/*.TEIP5.xml`
 do
 	echo "Add @xml:id to all new elements"
@@ -9,13 +10,13 @@ do
 	saxon -s:"$f" -xsl:https://openarabicpe.github.io/tools/xslt/generate_sitemap.xsl
   	echo "Update / generate bibliographic metadata"
   	echo "MODS"
-  	saxon -s:"$f" -xsl:https://openarabicpe.github.io/convert_tei-to-bibliographic-data/xslt/convert_tei-to-mods_issues.xsl
+  	saxon -s:"$f" -xsl:https://openarabicpe.github.io/convert_tei-to-bibliographic-data/xslt/convert_tei-to-mods_issues.xsl p_github-action=true
   	saxon -s:"$f" -xsl:https://openarabicpe.github.io/convert_tei-to-bibliographic-data/xslt/convert_tei-to-mods_articles.xsl
   	echo "BibTeX"
-  	saxon -s:"$f" -xsl:https://openarabicpe.github.io/convert_tei-to-bibliographic-data/xslt/convert_tei-to-bibtex_issues.xsl
+  	saxon -s:"$f" -xsl:https://openarabicpe.github.io/convert_tei-to-bibliographic-data/xslt/convert_tei-to-bibtex_issues.xsl p_github-action=true
   	saxon -s:"$f" -xsl:https://openarabicpe.github.io/convert_tei-to-bibliographic-data/xslt/convert_tei-to-bibtex_articles.xsl
   	echo "YAML"
-  	saxon -s:"$f" -xsl:https://openarabicpe.github.io/convert_tei-to-bibliographic-data/xslt/convert_tei-to-yaml_issues.xsl
+  	saxon -s:"$f" -xsl:https://openarabicpe.github.io/convert_tei-to-bibliographic-data/xslt/convert_tei-to-yaml_issues.xsl p_github-action=true
   	echo "Zotero RDF"
-  	saxon -s:"$f" -xsl:https://openarabicpe.github.io/convert_tei-to-bibliographic-data/xslt/convert_tei-to-zotero-rdf_issues.xsl
+  	saxon -s:"$f" -xsl:https://openarabicpe.github.io/convert_tei-to-bibliographic-data/xslt/convert_tei-to-zotero-rdf_issues.xsl p_github-action=true
 done
