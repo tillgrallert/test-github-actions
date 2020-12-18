a187c8b6b72c3bd5eb2de52366faeb3ed87e6865
Till Grallert <till.grallert@fu-berlin.de>
2020-12-18 18:13:51 +0100

    improved readability

--- .github/actions/attributes-bibliographic-metadata/process.sh
+++ .github/actions/attributes-bibliographic-metadata/process.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 # this selector is ignorant of the triggering file. The action will run on all files not just the ones recently changed
-for f in `ls tei/oclc_*.TEIP5.xml`
+for file in `ls tei/oclc_*.TEIP5.xml`
 do
 	echo "Add @xml:id to all new elements"
-	saxon -s:"$f" -xsl:https://openarabicpe.github.io/tools/xslt/tei_generate-xml-id.xsl -o:"$f" p_id-editor='github'
+	saxon -s:"$file" -xsl:https://openarabicpe.github.io/tools/xslt/tei_generate-xml-id.xsl -o:"$file" p_id-editor='github'
 	echo "Add @xml:lang to all new elements"
-	saxon -s:"$f" -xsl:https://openarabicpe.github.io/tools/xslt/tei_generate-xml-lang.xsl -o:"$f" p_id-editor='github'
+	saxon -s:"$file" -xsl:https://openarabicpe.github.io/tools/xslt/tei_generate-xml-lang.xsl -o:"$file" p_id-editor='github'
 	echo "Update sitemap.xml for Google's programmable search"
-	saxon -s:"$f" -xsl:https://openarabicpe.github.io/tools/xslt/generate_sitemap.xsl
+	saxon -s:"$file" -xsl:https://openarabicpe.github.io/tools/xslt/generate_sitemap.xsl
   	echo "Update / generate bibliographic metadata"
   	echo "MODS"
-  	saxon -s:"$f" -xsl:https://openarabicpe.github.io/convert_tei-to-bibliographic-data/xslt/convert_tei-to-mods_issues.xsl p_github-action=true
-  	saxon -s:"$f" -xsl:https://openarabicpe.github.io/convert_tei-to-bibliographic-data/xslt/convert_tei-to-mods_articles.xsl p_github-action=true
+  	saxon -s:"$file" -xsl:https://openarabicpe.github.io/convert_tei-to-bibliographic-data/xslt/convert_tei-to-mods_issues.xsl p_github-action=true
+  	saxon -s:"$file" -xsl:https://openarabicpe.github.io/convert_tei-to-bibliographic-data/xslt/convert_tei-to-mods_articles.xsl p_github-action=true
   	echo "BibTeX"
-  	saxon -s:"$f" -xsl:https://openarabicpe.github.io/convert_tei-to-bibliographic-data/xslt/convert_tei-to-bibtex_issues.xsl p_github-action=true
-  	saxon -s:"$f" -xsl:https://openarabicpe.github.io/convert_tei-to-bibliographic-data/xslt/convert_tei-to-bibtex_articles.xsl p_github-action=true
+  	saxon -s:"$file" -xsl:https://openarabicpe.github.io/convert_tei-to-bibliographic-data/xslt/convert_tei-to-bibtex_issues.xsl p_github-action=true
+  	saxon -s:"$file" -xsl:https://openarabicpe.github.io/convert_tei-to-bibliographic-data/xslt/convert_tei-to-bibtex_articles.xsl p_github-action=true
   	echo "YAML"
-  	saxon -s:"$f" -xsl:https://openarabicpe.github.io/convert_tei-to-bibliographic-data/xslt/convert_tei-to-yaml_issues.xsl p_github-action=true
+  	saxon -s:"$file" -xsl:https://openarabicpe.github.io/convert_tei-to-bibliographic-data/xslt/convert_tei-to-yaml_issues.xsl p_github-action=true
   	echo "Zotero RDF"
-  	saxon -s:"$f" -xsl:https://openarabicpe.github.io/convert_tei-to-bibliographic-data/xslt/convert_tei-to-zotero-rdf_issues.xsl p_github-action=true
+  	saxon -s:"$file" -xsl:https://openarabicpe.github.io/convert_tei-to-bibliographic-data/xslt/convert_tei-to-zotero-rdf_issues.xsl p_github-action=true
 done
